f40c91ac2d58360d77a5d03823e7bfa8de3da841
Damilare Stephen <stephen_damilare@yahoo.com>
2022-07-18 18:15:12 +0100

    Setup

--- cient/src/app/app.component.spec.ts
+++ cient/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'client'`, () => {
+  it(`should have as title 'cient'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
+    expect(app.title).toEqual('cient');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('client app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cient app is running!');
   });
 });
